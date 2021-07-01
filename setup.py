0018942f5f620a960b1d9c79b1cc904e01c8d1f5
Dmytro Khavro <dmytro_khavro@hotmail.com>
2021-07-01 23:05:19 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='probelyLearn',
+    version='0.0.1',
+    packages=['api', 'api.migrations', 'probely'],
+    url='',
+    license='MIT License',
+    author='Khavro',
+    author_email='dmytro@probely.com',
+    description='CreatingAPI'
+)
