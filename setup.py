7eb78c5bf9bf8a0e5281f74b634d061d25e4baec
Dmytro Khavro <dmytro_khavro@hotmail.com>
2021-07-02 12:39:43 +0100

    Adding README file

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(
     name='probelyLearn',
     version='0.0.1',
     packages=['api', 'api.migrations', 'probely'],
-    url='',
     license='MIT License',
     author='Khavro',
     author_email='dmytro@probely.com',
     description='CreatingAPI',
-    keywords=['python', 'api', 'probely', 'naointeressa']
+    keywords=['python', 'api', 'probely']
 )
